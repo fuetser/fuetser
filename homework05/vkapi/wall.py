e1a8b2b3a820c2390f6deac8f653758d23da462f
Alexander Zakharchuk <mystang.pro@gmail.com>
2022-11-25 19:16:50 +0300

    done

--- homework05/vkapi/wall.py
+++ homework05/vkapi/wall.py
@@ -9,6 +9,19 @@ from vkapi import config, session
 from vkapi.exceptions import APIError
 
 
+def get_posts_2500(
+    owner_id: str = "",
+    domain: str = "",
+    offset: int = 0,
+    count: int = 10,
+    max_count: int = 2500,
+    filter: str = "owner",
+    extended: int = 0,
+    fields: tp.Optional[tp.List[str]] = None,
+) -> tp.Dict[str, tp.Any]:
+    return {}
+
+
 def get_wall_execute(
     owner_id: str = "",
     domain: str = "",
